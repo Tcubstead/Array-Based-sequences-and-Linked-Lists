0e3e36c198130b1b4a12f53b158593c2a1c16f21
ThomasC <dradar77@gmail.com>
2025-10-23 11:40:36 -0400

    created the class that converts infix expressions to postfix expressions

--- Array_Based_sequences_and_Linked_Lists.py
+++ Array_Based_sequences_and_Linked_Lists.py
@@ -36,3 +36,48 @@ class stack:
 
     def __str__(self):
         return str(self._items)
+
+#class to convert infix expressions to postfix expressions
+class InfixConverter:
+    def __init__(self):
+        self.stack = stack()
+        self.precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '/': 2}
+
+    def convert(self, expression):
+        self.stack.clear()
+        postfix = []
+        tokens = expression.split()
+
+        for token in tokens:
+            if self._is_operand(token):
+                postfix.append(token)
+            elif token == '(':
+                self.stack.push(token)
+            elif token ==')':
+                while not self.stack.is_empty() and self.stack.peek() != '(':
+                    postfix.append(self.stack.pop())
+                self.stack.pop()
+            elif self._is_operator(token):
+                while (not self.stack.is_empty() and
+                       self.stack.peek() != '(' and
+                       self._has_precedence(self.stack.peek(), token)):
+                    postfix.append(self.stack.pop())
+                self.stack.push(token)
+
+        while not self.stack.is_empty():
+            postfix.append(self.stack.pop())
+
+        return ' '.join(postfix)
+
+    def _is_operand(self, token):
+        return token.isalum()
+
+    def _is_operator(self, token):
+        return token in self.precedence
+
+    def _has_precedence(self, op1, op2):
+        return self.precedence(op1, 0) >= self.precednce.get(op2, 0)
+
+
+
+        
